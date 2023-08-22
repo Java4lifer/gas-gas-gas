4d348747637d087c77073a47152a2bd013fbde41
Java4lifer <104568501+Java4lifer@users.noreply.github.com>
2023-08-22 14:01:17 +0000

    did shit, now fix shit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,57 @@
+
+import { useState, FormEvent } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import logo from './assets/aki-mino.jpg'
+
+
+/*
+Calculo: alccol/gasolina
+Se resultado menor 0.7 comepnsa Álcool */
+
+
+interface resultadoP{
+  titulo: string;
+  gasolina: string | number;
+  alcool: string | number;
+}
+ function App() {
+  const [gasolinaInput, setGasolinaInput] = useState(0)
+  const [alcoolInput, setAlcoolInput] = useState(0)
+  const [resultado, setResultado] = useState <resultadoP>()
+
+
+  function calculo(event: FormEvent) {
+    event.preventDefault();
+    let conta = (alcoolInput/gasolinaInput)
+    console.log(conta)
+  if(conta <= 0.7){
+    setResultado({
+      titulo:"Compensa usar Álcool",
+      gasolina: formataMoeda(gasolinaInput),
+      alcool: formataMoeda(alcoolInput)
+    })
+  }else {
+      setResultado({
+        titulo:"Compensa usar Gasolina",
+        gasolina: formataMoeda(gasolinaInput),
+        alcool: formataMoeda(alcoolInput)
+      })
+    }
+  }
+  function formataMoeda(valor:number){
+    let valorFormatado = valor.toLocaleString("pt-br",
+    {
+      style: "currency",
+      currency: "BRL"
+    })
+    return valorFormatado;
+    }
+     return <main className='container'>
+        <Image className="logo" src={logo} alt='Imagem foda'/>
+        <h1 className="titulo">Which is gooder?</h1>
+     </main>
+  }
+ 
+export default App
